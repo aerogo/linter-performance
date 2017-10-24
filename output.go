1f0e2ce82f8417eb09cac2d105ac97cde6d4b42b
Eduard Urbach <e.urbach@gmail.com>
2017-10-25 00:15:48 +0200

    Initial commit

---
+++ output.go
@@ -0,0 +1,39 @@
+package performance
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+// printResult shows the result for a route on the terminal.
+func printResult(label string, responseTime int64, responseSize float64) {
+	faint := color.New(color.Faint).SprintFunc()
+
+	// Response size color
+	var responseSizeColor func(a ...interface{}) string
+
+	switch {
+	case responseSize < 15:
+		responseSizeColor = color.New(color.FgGreen).SprintFunc()
+	case responseSize < 100:
+		responseSizeColor = color.New(color.FgYellow).SprintFunc()
+	default:
+		responseSizeColor = color.New(color.FgRed).SprintFunc()
+	}
+
+	// Response time color
+	var responseTimeColor func(a ...interface{}) string
+
+	switch {
+	case responseTime < 10:
+		responseTimeColor = color.New(color.FgGreen).SprintFunc()
+	case responseTime < 100:
+		responseTimeColor = color.New(color.FgYellow).SprintFunc()
+	default:
+		responseTimeColor = color.New(color.FgRed).SprintFunc()
+	}
+
+	// Output
+	fmt.Printf("%-67s %s %s %s %s\n", color.BlueString(label), responseSizeColor(fmt.Sprintf("%6.0f", responseSize)), faint("KB"), responseTimeColor(fmt.Sprintf("%7d", responseTime)), faint("ms"))
+}
---
+++ performance.go
@@ -0,0 +1,31 @@
+package performance
+
+import (
+	"time"
+
+	"github.com/aerogo/http/client"
+)
+
+// Linter ...
+type Linter struct {
+	start map[string]time.Time
+}
+
+// New creates a new performance linter.
+func New() *Linter {
+	return &Linter{
+		start: make(map[string]time.Time),
+	}
+}
+
+// Begin is called before a page is requested.
+func (linter *Linter) Begin(route string, uri string) {
+	linter.start[route] = time.Now()
+}
+
+// End is called after the page has responded.
+func (linter *Linter) End(route string, uri string, response client.Response) {
+	responseTime := time.Since(linter.start[route]).Nanoseconds() / 1000000
+	responseSize := float64(len(response.RawBytes())) / 1024
+	printResult(route, responseTime, responseSize)
+}
