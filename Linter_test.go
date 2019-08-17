e0e828939a59a34786904fa8246f64d8bbec5dcb
Eduard Urbach <e.urbach@gmail.com>
2019-08-17 12:17:41 +0900

    Updated dependencies

--- Linter_test.go
+++ Linter_test.go
@@ -5,14 +5,14 @@ import (
 
 	"github.com/aerogo/http/client"
 	performance "github.com/aerogo/linter-performance"
-	"github.com/stretchr/testify/assert"
+	"github.com/akyoto/assert"
 )
 
 func Test(t *testing.T) {
 	linter := performance.New()
 	linter.Begin("eduardurbach.com", "eduardurbach.com")
 	response, err := client.Get("https://eduardurbach.com").End()
-	assert.NoError(t, err)
+	assert.Nil(t, err)
 	assert.NotNil(t, response)
 	linter.End("eduardurbach.com", "eduardurbach.com", response)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,7 @@ module github.com/aerogo/linter-performance
 go 1.12
 
 require (
-	github.com/aerogo/http v1.0.6
-	github.com/akyoto/color v1.8.4
-	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/stretchr/testify v1.3.0
-	golang.org/x/sys v0.0.0-20190523142557-0e01d883c5c5 // indirect
+	github.com/aerogo/http v1.0.8
+	github.com/akyoto/assert v0.1.2
+	github.com/akyoto/color v1.8.7
 )
